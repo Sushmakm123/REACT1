98bcb587dc2f61f31d7f8f7f5e84d3b1e712cfca
sushmakm123 <kmsushma95@gmai.com>
2023-02-24 19:25:35 +0530

    done

---
+++ try.js
@@ -0,0 +1,26 @@
+// let red=10;
+// const blue=20;
+// let green =345;
+// console.log(red);
+// console.log(green);
+// console.log(pink);
+// var purple=360;
+// console.log(purole);
+
+// let dog="bark";
+// const cat="mew"
+// console.log(cat);
+// var monkey=420;
+// let cow="hamba";
+// console.log(cat);
+// console.log(monkey);
+// console.log(cow);
+// console.log(dog);
+// console.log(lion);
+// console.log(this);
+
+// alert("hello! i am a alert box!!")
+
+// confirm("press a button ok or cancel")
+
+prompt("please enter your name")
