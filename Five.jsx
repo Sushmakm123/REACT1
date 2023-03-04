535e59530a829e7187a69c72daffd6253d0c093e
Sushmakm123 <120468885+Sushmakm123@users.noreply.github.com>
2023-03-04 19:08:32 +0530

    Add files via upload

---
+++ Five.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Component } from 'react'
+class Five extends Component
+{
+    render(){
+        return(
+            <h1>internetexplorer</h1>
+        )
+    }
+}
+export default Five;
