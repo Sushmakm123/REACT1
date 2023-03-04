72a1bd8a7210aebd414a422c370c25c939f45f63
Sushmakm123 <120468885+Sushmakm123@users.noreply.github.com>
2023-03-04 19:37:12 +0530

    Add files via upload

---
+++ Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Logo = ()=>{
+    return(
+        <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Q638_noun_23486_ccIlsurAptukov_music.svg/800px-Q638_noun_23486_ccIlsurAptukov_music.svg.png" alt="" />
+        </div>
+    )
+}
+export default Logo
