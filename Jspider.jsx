55cbf840a889ecfc12e60d16b2e4547272cd3bfd
Sushmakm123 <120468885+Sushmakm123@users.noreply.github.com>
2023-03-05 12:47:19 +0530

    Add files via upload

---
+++ Jspider.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './jspider.css';
+const Abc=()=> 
+{
+return(
+    <div>
+        <nav className="open">
+    <img src="https://www.hoteljob.vn/files/untitled%20folder/bakery-la-gi.jpg" alt="logo" />
+ <div className="ok">
+   <ul>
+   <li>
+   <a  href="#">Home</a>
+   </li>
+   <li>
+   <a href="#">About</a>
+   </li>
+   <li>
+   <a href="#">Service</a>
+   </li>
+   </ul>
+</div>
+   <a  className="cta" href="#"><button>contact</button></a>
+   </nav>
+    </div>
+   
+)
+}
+export default Abc
