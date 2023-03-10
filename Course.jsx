945d9e1b8bbf1b6266eeb5550d01c24355eab828
Sushmakm123 <120468885+Sushmakm123@users.noreply.github.com>
2023-03-10 08:55:37 +0530

    cards

---
+++ Course.jsx
@@ -0,0 +1,157 @@
+import React from 'react'
+const Course=(props)=>{
+    return(
+<div className="parentBlock">
+    <div className="midBlock">
+    <div className="mainBlock">
+        <img src={props.payload[0].studentphoto} alt="" />
+        <h2>{props.payload[0].studentName}</h2>
+        <h2>{props.payload[0].courseName}</h2>
+        <h3>{props.payload[0].skills}</h3>
+        <h4>{props.payload[0].duration}</h4>
+    </div>
+    <div className="mainBlock">
+        <img src={props.payload[1].studentphoto} alt="" />
+        <h2>{props.payload[1].studentName}</h2>
+        <h2>{props.payload[1].courseName}</h2>
+        <h3>{props.payload[1].skills}</h3>
+        <h4>{props.payload[1].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[2].studentphoto} alt="" />
+        <h2>{props.payload[2].studentName}</h2>
+        <h2>{props.payload[2].courseName}</h2>
+        <h3>{props.payload[2].skills}</h3>
+        <h4>{props.payload[2].duration}</h4>
+     </div>
+     <div className="mainBlock">
+     <img src={props.payload[3].studentphoto} alt="" />
+        <h2>{props.payload[3].studentName}</h2>
+        <h2>{props.payload[3].courseName}</h2>
+        <h3>{props.payload[3].skills}</h3>
+        <h4>{props.payload[3].duration}</h4>
+     </div>
+     <div className="mainBlock">
+     <img src={props.payload[4].studentphoto} alt="" />
+        <h2>{props.payload[4].studentName}</h2>
+        <h2>{props.payload[4].courseName}</h2>
+        <h3>{props.payload[4].skills}</h3>
+        <h4>{props.payload[4].duration}</h4>
+     </div>
+     <div className="mainBlock">
+     <img src={props.payload[5].studentphoto} alt="" />
+        <h2>{props.payload[5].studentName}</h2>
+        <h2>{props.payload[5].courseName}</h2>
+        <h3>{props.payload[5].skills}</h3>
+        <h4>{props.payload[5].duration}</h4>
+     </div>
+     <div className="mainBlock">
+     <img src={props.payload[6].studentphoto} alt=""/>
+        <h2>{props.payload[6].studentName}</h2>
+        <h2>{props.payload[6].courseName}</h2>
+        <h3>{props.payload[6].skills}</h3>
+        <h4>{props.payload[6].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[7].studentphoto} alt="" />
+        <h2>{props.payload[7].studentName}</h2>
+        <h2>{props.payload[7].courseName}</h2>
+        <h3>{props.payload[7].skills}</h3>
+        <h4>{props.payload[7].duration}</h4>
+     </div>
+     <div className="mainBlock">
+     <img src={props.payload[8].studentphoto} alt="" />
+        <h2>{props.payload[8].studentName}</h2>
+        <h2>{props.payload[8].courseName}</h2>
+        <h3>{props.payload[8].skills}</h3>
+        <h4>{props.payload[8].duration}</h4>
+    </div>
+    <div className="mainBlock">
+        <img src={props.payload[9].studentphoto} alt="" />
+        <h2>{props.payload[9].studentName}</h2>
+        <h2>{props.payload[9].courseName}</h2>
+        <h3>{props.payload[9].skills}</h3>
+        <h4>{props.payload[9].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[10].studentphoto} alt="" />
+        <h2>{props.payload[10].studentName}</h2>
+        <h2>{props.payload[10].courseName}</h2>
+        <h3>{props.payload[10].skills}</h3>
+        <h4>{props.payload[10].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[11].studentphoto} alt="" />
+        <h2>{props.payload[11].studentName}</h2>
+        <h2>{props.payload[11].courseName}</h2>
+        <h3>{props.payload[11].skills}</h3>
+        <h4>{props.payload[11].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[12].studentphoto} alt="" />
+        <h2>{props.payload[12].studentName}</h2>
+        <h2>{props.payload[12].courseName}</h2>
+        <h3>{props.payload[12].skills}</h3>
+        <h4>{props.payload[12].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[13].studentphoto} alt="" />
+        <h2>{props.payload[13].studentName}</h2>
+        <h2>{props.payload[13].courseName}</h2>
+        <h3>{props.payload[13].skills}</h3>
+        <h4>{props.payload[13].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[14].studentphoto} alt="" />
+        <h2>{props.payload[14].studentName}</h2>
+        <h2>{props.payload[14].courseName}</h2>
+        <h3>{props.payload[14].skills}</h3>
+        <h4>{props.payload[14].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[15].studentphoto} alt="" />
+        <h2>{props.payload[15].studentName}</h2>
+        <h2>{props.payload[15].courseName}</h2>
+        <h3>{props.payload[15].skills}</h3>
+        <h4>{props.payload[15].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[16].studentphoto} alt="" />
+        <h2>{props.payload[16].studentName}</h2>
+        <h2>{props.payload[16].courseName}</h2>
+        <h3>{props.payload[16].skills}</h3>
+        <h4>{props.payload[16].duration}</h4>
+    </div>
+     <div className="mainBlock">
+     <img src={props.payload[17].studentphoto} alt="" />
+        <h2>{props.payload[17].studentName}</h2>
+        <h2>{props.payload[17].courseName}</h2>
+        <h3>{props.payload[17].skills}</h3>
+        <h4>{props.payload[17].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[18].studentphoto} alt="" />
+        <h2>{props.payload[18].studentName}</h2>
+        <h2>{props.payload[18].courseName}</h2>
+        <h3>{props.payload[18].skills}</h3>
+        <h4>{props.payload[18].duration}</h4>
+     </div>
+     <div className="mainBlock">
+     <img src={props.payload[19].studentphoto} alt="" />
+        <h2>{props.payload[19].studentName}</h2>
+        <h2>{props.payload[19].courseName}</h2>
+        <h3>{props.payload[19].skills}</h3>
+        <h4>{props.payload[19].duration}</h4>
+    </div>
+    <div className="mainBlock">
+    <img src={props.payload[20].studentphoto} alt="" />
+        <h2>{props.payload[20].studentName}</h2>
+        <h2>{props.payload[20].courseName}</h2>
+        <h3>{props.payload[20].skills}</h3>
+        <h4>{props.payload[20].duration}</h4>
+    </div>
+  </div>
+</div>
+)
+}
+export default Course;
