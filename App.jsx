fd9da4ea98807a376234e2549a879e4ab833d955
Sushmakm123 <120468885+Sushmakm123@users.noreply.github.com>
2023-03-17 22:10:15 +0530

    nonsynthetic

--- App.jsx
+++ App.jsx
@@ -1,27 +1,25 @@
-import React,{useState} from 'react'
-import './functioncounter.css'
-const App=()=>{
-  let [count,setCount]=useState(0)
-  let handleIncrement=()=>{
-    setCount(count +1)
-  }
-  let handleDecrement=()=>{
-setCount(count -1)
-  }
-let handleReset=()=>{
-  setCount(0)
+import React, { Component } from 'react'
+
+export default class App extends Component {
+constructor(){
+    super()
+    this.state={
+        username:"upendra"
+    }
 }
-return(
-<>
-<div className='mainblock'>
-  <h1>{count}</h1>
-<div className='subblock'>
-  <button className='a'   onClick={handleIncrement}>increment +</button>
-  <button className='b' onClick={handleDecrement}>decrement -</button>
-  <button className='c' onClick={handleReset}>reset</button>
-</div>
-</div>
-</>
-)
-} 
-export default App;
+componentDidMount()
+{
+    let btn=document.querySelector('button')
+    btn.addEventListener('click',()=>{
+        this.setState({username:this.state.username="KGF"})
+    })
+}
+render() {
+    return (
+        <>
+      <div>{this.state.username}</div>
+      <button>change</button>
+      </>
+    )
+  }
+}
