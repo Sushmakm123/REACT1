b559e8d8a4a2b137e0b60b58e5fd1c1c46755493
Sushmakm123 <120468885+Sushmakm123@users.noreply.github.com>
2023-03-19 12:04:44 +0530

    href

--- App.jsx
+++ App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-const App=()=>{
-    let handlesubmit=()=>{
-        toast.success("logged in successfully",{position:toast.POSITION.TOP_CENTER})
+import React from "react";
+import { Component,createRef } from "react";
+class App extends Component{
+    constructor(){
+        super()
+        this.state={
+            name:"hello world!"}
+        this.h1ref=createRef()
+        
     }
-
-return(
-    <div>
-        <ToastContainer/>
-        <button onClick={handlesubmit}>click for message</button>
-    </div>
-)
+    componentDidMount(){
+        let h1=document.querySelector('h1')
+        console.log(h1,"DOM WAY");
+        console.log(this.h1ref.current,"React way");
+    }
+    render(){
+        return(
+            <>
+            <h1 ref={this.h1ref}>{this.state.name}</h1>
+            </>
+        )
+    }
 }
 export default App;
